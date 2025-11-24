0299011a1f4bac8517895a9c73593d2841ca912a
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-11-24 15:58:27 +0530

    REPL & Global Objects Practice

---
+++ Ch - 2 REPL & Global Objects/simpleTimer.js
@@ -0,0 +1,19 @@
+let seconds = 30;
+
+setInterval(() => {
+  let minute = Math.floor(seconds / 60);
+  let sec = seconds % 60;
+  if (seconds == -1) return;
+
+  if (minute <= 9) {
+    if (sec <= 9) {
+      console.log(`0${minute} : 0${sec}`);
+    } else {
+      console.log(`${minute} : ${sec}`);
+    }
+  } else {
+    console.log(`${minute} : ${sec}`);
+  }
+
+  seconds--;
+}, 1000);
---
+++ Ch - 2 REPL & Global Objects/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+console.log(`Path of this file : ${__filename}`);
+// console.log(`Path of this folder : ${__dirname}`);
+
+// setTimeout(() => {
+//   console.log(`Delay for 2 second`);
+// }, 2000 /* millisecond*/);
+
+// let num = 1;
+// setInterval(() => {
+//   console.log(`1 second interval ${num++}`);
+// }, 1000);
