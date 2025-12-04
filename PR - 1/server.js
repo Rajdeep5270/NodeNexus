d7c18f8c81d59b2d2a368591918b18a0b0ed1ded
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-04 20:33:34 +0530

    PR - 1 Custom server

--- PR - 1/server.js
+++ PR - 1/server.js
@@ -1,15 +1,44 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    res.write("Server is on.\n");
-    res.end("Server loader is off.");
-});
 
-server.listen(9090, (e) => {
-    if (e) {
-        console.log("Server is not started.");
+    if (req.url === '/favicon.ico')
         return;
+
+    const msg = `New User : ${new Date().toLocaleString()} Page : ${req.url} IP Address : ${res.socket.remoteAddress}\n`
+    fs.appendFile('./log.txt', msg, (e) => { });
+
+    let filename = "";
+
+    switch (req.url) {
+        case '/':
+            filename = "index.html";
+            break;
+
+        case '/about':
+            filename = "about.html";
+            break;
+
+        case '/contact':
+            filename = "contact.html"
+            break;
+
+        default:
+            filename = "404.html"
     }
 
-    console.log("Server is started.");
+    // res.write("<h1>Welcome to my server.</h1>");
+    // res.end("This is my first server.");
+
+    fs.readFile(filename, (err, result) => {
+        res.end(result);
+    })
+});
+
+server.listen(1200, (e) => {
+    if (e) {
+        console.log("Server is not started", e);
+    }
+    console.log("Server is Started");
 });
