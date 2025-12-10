b4afd81d9e2cdc21acd935108867d42f56ade32c
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-10 14:36:42 +0530

    git add .

--- PR - 3/server.js
+++ PR - 3/server.js
@@ -5,11 +5,66 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded());
 app.use('/css', express.static(__dirname + '/css'));
+
+let usersData = [
+    {
+        id: 234,
+        name: "Rajdeep Singh Shekhawat",
+        profileName: "rajdeep5270",
+        email: "rajdeepex5270@gmail.com",
+        password: "Rajdeep5270ex@"
+    },
+    {
+        id: 578,
+        name: "Sagar Chavda",
+        profileName: "sagarChavda@",
+        email: "sagar9922@gmail.com",
+        password: "sagar1234"
+    },
+    {
+        id: 152,
+        name: "Rahul Makwana",
+        profileName: "rahul@1234",
+        email: "rm993322@gmail.com",
+        password: "rahul9099"
+    },
+]
 
 app.get("/", (req, res) => {
     res.render("login");
+});
+
+app.get("/registerPage", (req, res) => {
+    res.render("registerPage");
+});
+
+app.get("/backUser", (req, res) => {
+    res.redirect("/");
+});
+
+app.get("/userData", (req, res) => {
+    res.redirect("userData");
 })
+
+app.post("/addUser", (req, res) => {
+    let user = req.body;
+
+    console.log(user);
+
+    res.render("userData", {
+        usersData
+    })
+});
+
+app.post("/loginUser", (req, res) => {
+    let userLoginInfo = req.body;
+
+    console.log(userLoginInfo);
+
+    res.render("userData");
+});
 
 app.listen(PORT, (e) => {
     if (e) {
