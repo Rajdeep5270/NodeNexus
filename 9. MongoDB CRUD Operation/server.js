3f92f617a78edad452310697950e49af5ed1707c
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-18 15:39:41 +0530

    Update Logic in Book Management Project

--- 9. MongoDB CRUD Operation/server.js
+++ 9. MongoDB CRUD Operation/server.js
@@ -9,9 +9,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 
+// ("/") page route 
 app.get("/", (req, res) => {
     bookModel.find()
         .then((allBooks) => {
+            if (allBooks.length == 0) {
+                return res.render("form");
+            }
             res.render("view", { allBooks });
         })
         .catch(e => {
@@ -19,10 +23,12 @@ app.get("/", (req, res) => {
         });
 })
 
+// form page route 
 app.get("/addBookPage", (req, res) => {
     res.render("form");
 })
 
+// add book logic and route to ("/")
 app.post("/addBook", (req, res) => {
 
     bookModel.create(req.body).then(() => {
@@ -34,6 +40,43 @@ app.post("/addBook", (req, res) => {
     res.redirect("/");
 });
 
+// to edit and fill edit form logic 
+app.get("/editBook/:id", async (req, res) => {
+
+    // this is a long process 
+
+    // bookModel.find()
+    //     .then((allBooks) => {
+    //         console.log("Books fetched...");
+    //         // console.log(allBooks);
+    //         const findBook = allBooks.find(book => req.params.id == book.id);
+    //         // console.log(findBook);
+
+    //         res.render("edit", { findBook });
+    //     })
+    //     .catch(e => {
+    //         console.log("Books not fetched...");
+    //     });
+
+    // this is short and easy way using mongoose built-it methods findById is a built in method
+
+    const findBook = await bookModel.findById(req.params.id);
+
+    res.render("edit", { findBook });
+});
+
+// to update filled edit form 
+app.post("/updateBook", async (req, res) => {
+    // console.log(req.body.book_id);
+
+    const updatedBook = await bookModel.findByIdAndUpdate(req.body.book_id, req.body, { new: true });
+
+    // console.log(updatedBook);
+
+    res.redirect("/");
+});
+
+// delete book logic and route to("/")
 app.get("/deleteBook", (req, res) => {
     bookModel.findByIdAndDelete(req.query.bookId)
         .then(() => {
@@ -46,6 +89,7 @@ app.get("/deleteBook", (req, res) => {
     res.redirect("/");
 });
 
+// listen to port number 8080 
 app.listen(PORT, (e) => {
     if (e)
         console.log("Server is not started...", e);
