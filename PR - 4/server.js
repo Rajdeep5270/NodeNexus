50e9a57ecddd1c3056a64cf4872e991eeb359bf2
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-12-18 19:17:00 +0530

    PR - 4 Movie Management System

---
+++ PR - 4/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const PORT = 8080;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname));
+
+// rendered view page for first time website load 
+app.get("/", (req, res) => {
+    res.render("view");
+})
+
+
+// to start server 
+app.listen(PORT, e => {
+    if (e)
+        console.log("Server is not started...");
+
+    console.log("Server is started...");
+})
