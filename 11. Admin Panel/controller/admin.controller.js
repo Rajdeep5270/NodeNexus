cd485e1a5d7de44034cbf31d24d8adfbaaf04b02
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-01-13 13:45:15 +0530

    Admin Panel

--- 11. Admin Panel/controller/admin.controller.js
+++ 11. Admin Panel/controller/admin.controller.js
@@ -163,12 +163,31 @@ module.exports.verifyEmail = async (req, res) => {
     const info = await transporter.sendMail({
         from: 'Admin Panel <rajdeep11559@gmail.com>',
         to: req.body.email,
-        subject: 'Admin Panel OTP Testing',
-        html: `<h2 style:color='red'>OTP : ${OTP}</h2>`
-    })
+        subject: 'Verification Code: Admin Panel',
+        html: `
+    <div style="font-family: Helvetica, Arial, sans-serif; min-width: 1000px; overflow: auto; line-height: 2">
+      <div style="margin: 50px auto; width: 70%; padding: 20px 0">
+        <div style="border-bottom: 1px solid #eee">
+          <a href="" style="font-size: 1.4em; color: #00466a; text-decoration: none; font-weight: 600">Admin Panel</a>
+        </div>
+        <p style="font-size: 1.1em">Hi,</p>
+        <p>Thank you for choosing Admin Panel. Use the following OTP to complete your sign-in procedures. OTP is valid for 5 minutes.</p>
+        <h2 style="background: #00466a; margin: 0 auto; width: max-content; padding: 0 10px; color: #fff; border-radius: 4px;">
+          ${OTP}
+        </h2>
+        <p style="font-size: 0.9em;">Regards,<br />Admin Panel Team</p>
+        <hr style="border: none; border-top: 1px solid #eee" />
+        <div style="float: right; padding: 8px 0; color: #aaa; font-size: 0.8em; line-height: 1; font-weight: 300">
+          <p>Admin Panel Inc</p>
+          <p>India</p>
+        </div>
+      </div>
+    </div>
+    `
+    });
 
-    console.log(info.messageId);
-    return res.redirect('/');
+    // console.log(info.messageId);
+    return res.render('auth/otp-verification', { OTP });
 }
 
 // admin logout logic 
